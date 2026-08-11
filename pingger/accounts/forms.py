551672973a6568f42a2fbd0d70ff7d1545fa6bbe
Solanki777 <solankimaheshkhash7@gmail.com>
2026-08-11 21:16:44 +0530

    registration page is done

---
+++ pingger/accounts/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ["username","email","password1","password2"]
+
+    def clean_email(self):
+        email = self.cleaned_data["email"]
+
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError(
+                "An account with the same email is already exists."
+            )
+
+        return email
