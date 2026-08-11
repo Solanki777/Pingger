ebf8ef60ec49b260f2bebc32c428e3ca8f3dea42
Solanki777 <solankimaheshkhash7@gmail.com>
2026-08-11 21:29:09 +0530

    modification in forms

--- pingger/accounts/forms.py
+++ pingger/accounts/forms.py
@@ -3,11 +3,27 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class RegisterForm(UserCreationForm):
+    name = forms.CharField(max_length=150,required=True)
+
     email = forms.EmailField(required=True)
+
+    profession = forms.CharField(required=True,max_length=150)
+
+    company = forms.CharField(
+        max_length=150,
+        required=False
+    )
 
     class Meta:
         model = User
-        fields = ["username","email","password1","password2"]
+        fields = [
+            "name",
+            "email",
+            "profession",
+            "company",
+            "password1",
+            "password2",
+                  ]
 
     def clean_email(self):
         email = self.cleaned_data["email"]
