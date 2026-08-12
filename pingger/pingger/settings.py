962c99e7f188100726c7285b0fc87ffed038ddfa
Solanki777 <solankimaheshkhash7@gmail.com>
2026-08-12 11:21:36 +0530

    using postgress database for this project

--- pingger/pingger/settings.py
+++ pingger/pingger/settings.py
@@ -28,6 +28,22 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -74,9 +90,13 @@ WSGI_APPLICATION = "pingger.wsgi.application"
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pingger_db',
+        'USER': 'postgres',
+        'PASSWORD': 'i_am_the_admin7@',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
