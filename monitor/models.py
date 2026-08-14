d0c1ac9697ec1ca788f76402ca9d231b19c39ee8
Solanki777 <solankimaheshkhash7@gmail.com>
2026-08-14 23:33:23 +0530

    intro to database

--- monitor/models.py
+++ monitor/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Monitor(models.Model):
+
+    name = models.CharField(max_length=100)
+
+    url = models.URLField()
+
+    is_active = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
