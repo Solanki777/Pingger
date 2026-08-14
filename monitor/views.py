a3c2b8c86540daf9bbee746467983a9e9031a17c
Solanki777 <solankimaheshkhash7@gmail.com>
2026-08-14 23:19:58 +0530

    new attributes are added

--- monitor/views.py
+++ monitor/views.py
@@ -1,28 +1,36 @@
 from django.shortcuts import render
 import requests
+import time
 
 
 def ping(request):
 
     result = None
+    response_time = None
+    status_code = None
 
     if request.method == "POST":
 
         url = request.POST.get("url")
 
         try:
+            start_time = time.time()
             response = requests.get(url, timeout=10)
+            end_time = time.time()
 
-            result = (
-                f"Website is reachable! "
-                f"Status Code: {response.status_code}"
+            response_time = round(
+                (end_time-start_time) *1000,2
             )
+            status_code = response.status_code
+
+
+            result = "website is reachable"
 
         except requests.RequestException:
             result = "Website is down or unreachable."
 
     return render(request,"ping.html",
-                  {
-                      "result":result
-                      
-                  })
+                {
+                    "result":result,
+                    "status_code":status_code,
+                    "response_time":response_time })
