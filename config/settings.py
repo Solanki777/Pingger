82fba75f9101faaf894a51a29d631116c237820a
Solanki777 <solankimaheshkhash7@gmail.com>
2026-08-14 23:50:33 +0530

    switching database to postgres

--- config/settings.py
+++ config/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "pingger",
+        "USER": "postgres",
+        "PASSWORD": "i_am_the_admin7@",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
